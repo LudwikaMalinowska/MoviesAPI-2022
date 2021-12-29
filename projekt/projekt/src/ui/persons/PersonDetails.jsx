2fede8f4cbaf205e1d9621a344065d16fb662ad0
Ludwika Malinowska <malinowska.L@wp.pl>
2021-12-29 22:44:38 +0100

    Projekt - persons - wyswietlanie filmow w ktorych osoba jest rezyserem i w ktorych gra jako aktor, wraz z odnosnikami do movie details

--- projekt/projekt/src/ui/persons/PersonDetails.jsx
+++ projekt/projekt/src/ui/persons/PersonDetails.jsx
@@ -1,13 +1,70 @@
 import { connect } from "react-redux";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { getActorList } from "../../ducks/actors/operations";
+import { getAllActors } from "../../ducks/actors/selectors";
+import { getMovieList } from "../../ducks/movies/operations";
 import { deletePerson } from "../../ducks/persons/operations";
+import {getAllMovies} from "../../ducks/movies/selectors";
 
 
-const PersonDetails = ({person, deletePerson}, props) => {
+
+const PersonDetails = ({person, actors, movies, deletePerson}, props) => {
+
+    useEffect(() => {
+        getActorList();
+        getMovieList();
+    }, []);
 
     const handleDelete = () => {
         deletePerson(person);
         alert("usunięto")
+    }
+
+    
+
+    const directorIn = () => {
+        // console.log("movies: ", movies);
+        const moviesDirected = movies.filter(movie => {
+            
+            const isDir = movie.director_id ? 
+            (movie.director_id === person.id)
+            : false;
+
+            return isDir;
+        })
+        // console.log("movies directed: ", moviesDirected);
+
+        const moviesLiElements = moviesDirected.map(movie => {
+            const toLink = `/movies/${movie.id}`
+            return (
+                <li><Link to={toLink}>{movie.title}</Link></li>
+            )
+        })
+
+        return (<ul>{moviesLiElements}</ul>)
+    }
+
+    const actorIn = () => {
+        const actorData = actors.filter(actor => actor.person_id === person.id);
+        // console.log(actorData);
+        const moviesIn = movies.filter(movie => {
+            for (const data of actorData){
+                if (data.movie_id === movie.id)
+                    return true;
+            }
+            return false;
+        });
+        
+        const moviesLiElements = moviesIn.map(movie =>{
+            const toLink = `/movies/${movie.id}`
+            return (
+                <li><Link to={toLink}>{movie.title}</Link></li>
+            )
+        } )
+        // console.log(moviesIn);
+
+        return (<ul>{moviesLiElements}</ul>)
     }
 
     const editLink = `/persons/${person.id}/edit`
@@ -17,6 +74,16 @@ const PersonDetails = ({person, deletePerson}, props) => {
         <p>{person.id}</p>
         <p>{person.birth_date}</p>
         <p>{person.nationality}</p>
+
+        <ul>
+            Reżyser w filmach:
+            {directorIn()}
+        </ul>
+        <ul>
+            Aktor w filmach:
+            {actorIn()}
+
+        </ul>
 
         <Link to={editLink}><button>Edytuj</button></Link>
         <button onClick={handleDelete}>Usuń</button>
@@ -38,13 +105,17 @@ const mapStateToProps = (state, props) => {
     // console.log(state);
     const id = props.match.params.id;
     return {
-        person: state.entities.persons.byId[id]
+        person: state.entities.persons.byId[id],
+        movies: getAllMovies(state),
+        actors: getAllActors(state)
     };
     
 }
 
 const mapDispatchToProps = {
-    deletePerson
+    deletePerson,
+    getMovieList,
+    getActorList
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(PersonDetails);
