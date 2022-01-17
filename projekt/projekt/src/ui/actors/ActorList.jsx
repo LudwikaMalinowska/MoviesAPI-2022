c0bd54fe670716c85343404870652ca0a78825e9
Ludwika Malinowska <malinowska.L@wp.pl>
2022-01-17 23:29:29 +0100

    Projekt - fix - actors - poprawki w wyswietlaniu listy aktorów

--- projekt/projekt/src/ui/actors/ActorList.jsx
+++ projekt/projekt/src/ui/actors/ActorList.jsx
@@ -6,15 +6,19 @@ import { getActorList } from '../../ducks/actors/operations';
 import {getAllActors} from '../../ducks/actors/selectors';
 import { getAllPersons } from "../../ducks/persons/selectors";
 import { getPersonList } from '../../ducks/persons/operations';
+import { getMovieList } from '../../ducks/movies/operations';
+import { getAllMovies } from '../../ducks/movies/selectors';
 
 
-const ActorList = ({actors, persons, getActorList, getPersonList}, props) => {
+const ActorList = ({actors, movies, persons, getActorList, getMovieList, getPersonList}, props) => {
     const { t } = useTranslation();
     
     useEffect(() => {
         
         if (persons.length <= 1)
             getPersonList();
+        if (movies.length <= 1)
+            getMovieList();
         
         getActorList();
         
@@ -23,9 +27,12 @@ const ActorList = ({actors, persons, getActorList, getPersonList}, props) => {
     const actorList = actors ? (actors.map(actor => {
         const id = actor.person_id;
         const person = persons.find(person => person.id === id);
+        const movie = movies.find(movie => movie.id === actor.movie_id)
         const actorLink = `/persons/${actor.person_id}`
+        const movieLink = `/movies/${actor.movie_id}`
         return (<li key={actor.id}>
             <p>{person.first_name} {person.last_name}</p>
+            <p className='movie-link'><Link to={movieLink}>{movie.title}</Link></p>
             <Link to={actorLink}><button>{t("details")}</button></Link>
 
         </li>
@@ -43,14 +50,16 @@ const ActorList = ({actors, persons, getActorList, getPersonList}, props) => {
 const mapStateToProps = (state) => {
     return {
         actors: getAllActors(state),
-        persons: getAllPersons(state)
+        persons: getAllPersons(state),
+        movies: getAllMovies(state)
     };
     
 }
 
 const mapDispatchToProps = {
     getActorList,
-    getPersonList
+    getPersonList,
+    getMovieList
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ActorList);
